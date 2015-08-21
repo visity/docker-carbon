e93b819afdda3c057c159d2c2b36a7d570bfcdab
Jan Verhoeven <jan@visity.nl>
2015-08-21 11:01:54 +0200

    Run carbon cache as user. Added docker-entrypoint.sh script alike the ones from the official repos. Simplify.

---
+++ docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+# Environment vars always exist with defaults (see Dockerfile)
+sed 's#MAX_UPDATES_PER_SECOND = .*#MAX_UPDATES_PER_SECOND = '"$MAX_UPDATES_PER_SECOND"'#' -i /opt/graphite/conf/carbon.conf;
+sed 's#WHISPER_AUTOFLUSH = .*#WHISPER_AUTOFLUSH = '"$WHISPER_AUTOFLUSH"'#' -i /opt/graphite/conf/carbon.conf;
+
+# Add carbon as command if needed
+if [ "${1:0:1}" = '-' ]; then
+	set -- /opt/graphite/bin/carbon-cache.py "$@"
+fi
+
+# No need to do gosu (carbon takes care of stepping down from root)  
+if [ "$1" = '/opt/graphite/bin/carbon-cache.py' ]; then
+		chown -R carbon:carbon /opt/graphite
+fi
+
+# As argument is not related to gunicorn,
+# then assume that user wants to run his own process,
+# for example a `bash` shell to explore this image
+exec "$@"
